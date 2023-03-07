1e59d39008d60e826a5f99366f9ef4ccc0a6b84d
Abdullah Al Masrur <abdullahalmasrur8@gmail.com>
2023-03-07 12:03:27 +0600

    Update HomePage.dart

--- lib/Homepage/HomePage.dart
+++ lib/Homepage/HomePage.dart
@@ -1,12 +1,8 @@
+// ignore_for_file: file_names, use_key_in_widget_constructors, non_constant_identifier_names
+
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:ionicons/ionicons.dart';
-import 'package:ytlogin/Homepage/BottomLogOutPage.dart';
-import 'BottomHomePage.dart';
-import 'BottomMenuPage.dart';
-import 'BottomProfilePage.dart';
 
-final user = FirebaseAuth.instance.currentUser!;
 
 class HomePage extends StatefulWidget {
   @override
@@ -14,22 +10,17 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  int _currentIndex = 0;
 
-  final BottomPages = [
-    BottomHomePage(),
-    BottomMenuPage(),
-    BottomProfilePage(),
-    BottomLogOutPage(),
-  ];
+
   @override
   Widget build(BuildContext context) {
+    final user = FirebaseAuth.instance.currentUser!;
     return Scaffold(
       appBar: AppBar(
         title: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text(
+            const Text(
               'Welcome',
             ),
             Text(
@@ -38,49 +29,6 @@ class _HomePageState extends State<HomePage> {
             ),
           ],
         ),
-        actions: [
-          IconButton(
-            onPressed: () {},
-            icon: Icon(Ionicons.notifications_outline),
-          ),
-          IconButton(
-            onPressed: () {},
-            icon: Icon(Ionicons.search_outline),
-          )
-        ],
-      ),
-      body: BottomPages[_currentIndex],
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _currentIndex,
-        iconSize: 20,
-        selectedFontSize: 18,
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(Ionicons.home_outline),
-            label: 'Home',
-            backgroundColor: Colors.blue,
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Ionicons.menu_outline),
-            label: 'Menu',
-            backgroundColor: Colors.green,
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Ionicons.person_outline),
-            label: 'Profile',
-            backgroundColor: Colors.green,
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Ionicons.log_out_outline),
-            label: 'Log Out',
-            backgroundColor: Colors.red,
-          ),
-        ],
-        onTap: (index) {
-          setState(() {
-            _currentIndex = index;
-          });
-        },
       ),
     );
   }
